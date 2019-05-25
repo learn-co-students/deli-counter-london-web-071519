af3bffeaedf497d6e592115d57706bcb7a673416
Eliot Howes <ejhowes@gmail.com>
2019-05-25 09:50:25 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,23 @@
 # Write your code here.
+require 'pry'
+
+def line(katz_deli)
+  if katz_deli.empty? == true
+    puts "The line is currently empty."
+  else
+    counter = 1
+    current_line = []
+    katz_deli.each do |name|
+      current_line.push("#{counter}. #{name}")
+      counter += 1
+    end
+    puts "The line is currently: #{current_line.join(" ")}"
+  end
+end
+
+def take_a_number(katz_deli, name)
+  if katz_deli.empty?
+    katz_deli.push(name)
+  puts "Welcome, #{name}. You are number #{katz_deli.size} in line."
+  end
+end
